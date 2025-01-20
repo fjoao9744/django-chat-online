5f0b5f6a3e8f40c208e43adca8461d174d399211
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-01-20 19:27:36 -0300

    teste de static

--- api/settings.py
+++ api/settings.py
@@ -114,7 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Definindo o diretório 'static' na raiz do projeto
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
